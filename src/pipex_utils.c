d886ce6eebd3db3413d92a7d9a185d8d7d47bf19
Alvaro Miguel Gil <amiguel-@c1r14s2.42madrid.com>
2024-05-07 17:07:00 +0200

    subo utils

--- src/pipex_utils.c
+++ src/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: amiguel- <amiguel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 10:55:24 by amiguel-          #+#    #+#             */
-/*   Updated: 2024/05/07 15:11:53 by amiguel-         ###   ########.fr       */
+/*   Updated: 2024/05/07 17:05:52 by amiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,70 @@ int	file_open(char *file, int in_out)
 		exit(0);
 	return (ret);
 }
+
+void	ft_free_tab(char **tab)
+{
+	size_t	i;
+	
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	return (tab);
+}
+
+char	*get_env(char *name,  char **env)
+{
+	int		i;
+	int		j;
+	char	*sub;
+
+	i = 0;
+	while (env[i])
+	{
+		j = 0;
+		while (env[i][j] && env[i][j] != '=')
+			j++;
+		sub = ft_substr(env[i], 0, j);
+		if (ft_strcmp(sub, name) == 0)
+		{
+			free(sub);
+			return (env[i] + j + 1);
+		}
+		free(sub);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_path(char *cmd, char **env)
+{
+	int		i;
+	char	*exec;
+	char	**allpath;
+	char	*path_part;
+	char	**s_cmd;
+
+	i = -1;
+	allpath = ft_split(get_env("PATH", env), ':');
+	s_cmd = ft_split(cmd, ' ');
+	while (allpath[i++])
+	{
+		path_part = ft_strjoin(allpath[i], "/");
+		exec = ft_strjoin(path_part, s_cmd[0]);
+		free(path_part);
+		if (access(exec, F_OK | X_OK) == 0)
+		{
+			ft_free_tab(s_cmd);
+			return(exec);
+		}
+		free(exec);
+	}
+	ft_free_tab(allpath);
+	ft_free_tab(s_cmd);
+	return(cmd);
+}
+
+ 
